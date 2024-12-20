e367c5fa54cb49ce721c7c64f8eeff54d861e880
PetrSauer <petr.strelec@email.cz>
2024-12-20 22:42:43 +0100

    complete final version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [data, setData] = useState({})
+    const [url, setUrl] = useState("")
+
+    const getPositionData = async() => {
+        const response = await fetch("https://api.wheretheiss.at/v1/satellites/25544") //https://api.wheretheiss.at/v1/satellites/25544 http://api.open-notify.org/iss-now.json 
+        const dataPos = await response.json()
+        setData(dataPos)
+        setUrl(`https://mapy.cz/fnc/v1/showmap?mapset=outdoor&center=${dataPos["longitude"]},${dataPos["latitude"]}&zoom=10&marker=true`) // https://mapy.cz/zakladni?x=${dataPos["longitude"]}&y=${dataPos["latitude"]}&z=8
+
+    }
+
+    useEffect( () => {
+        getPositionData()
+    }, [])
+
+    if(data.timestamp) {
+        return (
+        <div>
+            <h1>ISS position</h1>
+            <p>Time: {new Date(data.timestamp*1000).toLocaleTimeString()}</p>
+            <p>Zeměpisná délka: {data.longitude}</p>
+            <p>Zeměpisná šířka: {data.latitude}</p>
+            <p><a href={url} target="_blank">Pozice ISS v mapách</a></p>
+            
+        </div>       
+    )
+    }
+    else {
+        return (
+            <div>
+                <h1>ISS position</h1>
+                <p>Načítání dat</p>
+            </div>
+        )
+    }
+    
 }
 
-export default App;
+export default App
